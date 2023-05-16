48c144dd150e254496671bc2cf7e2a7e65eb5566
Misha Filchagin <79020757+Defender-sec@users.noreply.github.com>
2023-05-16 22:37:25 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,43 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <string>
 template<typename T, int size>
 class TStack {
   // добавьте код стека
-};
+ private:
+    T arr[100];
+    int m;
 
+ public:
+    TStack() : m(-1) {}
+    void push(T value) {
+        if (!isFull())
+            arr[++m] = value;
+        else
+            throw std::string("Full");
+    }
+    T get() const {
+        return arr[m];
+    }
+    T pop() {
+        if (isEmpty())
+            throw std::string("Empty");
+        else
+            return arr[m--];
+    }
+    bool isEmpty() const {
+        return m == -1;
+    }
+    bool isFull() const {
+        return m == size - 1;
+    }
+    T top() {
+        if (!isEmpty()) {
+            return arr[m];
+        } else {
+            exit(EXIT_FAILURE);
+        }
+    }
+};
 #endif  // INCLUDE_TSTACK_H_
